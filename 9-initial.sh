e30ceaf871f02a33ef55e391647398995e2508d9
Jon Leahy <jleahy@propelfinance.co.uk>
2021-12-18 00:09:33 +0000

    initial

---
+++ 9-initial.sh
@@ -0,0 +1,23 @@
+#mkdir ~/dev/sync/
+#
+#cd ~/dev/sync
+#
+#git clone https://github.com/jonathanleahy/linux-tools-setup-initial.git
+#
+#cd linux-tools-setup-initial
+#
+#chmod 777 ./ -R
+#
+#./go.sh
+
+rm ~/dev/sync/work -rf
+
+mkdir ~/dev/sync/work
+
+cd ~/dev/sync/work
+
+git clone git@github.com:jonathanleahy/linux-tools-setup.git
+
+cd ~/dev/sync/work/linux-tools-setup
+
+echo "run setup"
