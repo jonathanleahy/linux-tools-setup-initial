95ba5454772e4ebf51a1877a385811fb50acf4bd
Jon Leahy <jleahy@propelfinance.co.uk>
2021-12-18 00:45:54 +0000

    added readme

--- 9-initial.sh
+++ 9-initial.sh
@@ -5,7 +5,8 @@ mkdir ~/dev/tools
 cd ~/dev/tools
 
 git clone git@github.com:jonathanleahy/local-tools.git
-cd ~/dev/tools/linux-tools
+
+cd ~/dev/tools/local-tools
 
 #git clone git@github.com:jonathanleahy/linux-tools-setup.git
 #cd ~/dev/tools/linux-tools-setup
