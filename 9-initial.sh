5145f195c5320baeb822d1ef176a4c10bb2182ad
Jon Leahy <jleahy@propelfinance.co.uk>
2021-12-18 00:32:33 +0000

    added readme

--- 9-initial.sh
+++ 9-initial.sh
@@ -1,25 +1,13 @@
-#mkdir ~/dev/sync/
-#
-#cd ~/dev/sync
-#
-#git clone https://github.com/jonathanleahy/linux-tools-setup-initial.git
-#
-#cd linux-tools-setup-initial
-#
-#chmod 777 ./ -R
-#
-#./go.sh
+rm ~/dev/tools -rf
 
-rm ~/dev/sync/work -rf
+mkdir ~/dev/tools
 
-mkdir ~/dev/sync/work
-
-cd ~/dev/sync/work
+cd ~/dev/tools
 
 git clone git@github.com:jonathanleahy/local-tools.git
 
 git clone git@github.com:jonathanleahy/linux-tools-setup.git
 
-cd ~/dev/sync/work/linux-tools-setup
+cd ~/dev/tools/linux-tools-setup
 
 echo "run setup"
